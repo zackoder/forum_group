de0c7822b42850d895c69f47aaa70f1720b53e16
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-06 21:12:43 +0100

    edit username (reje3tha machi unique)

--- models/init_tables.go
+++ models/init_tables.go
@@ -30,7 +30,7 @@ func UsersTable(db *sql.DB) {
 	query := `
 		CREATE TABLE IF NOT EXISTS users(
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			username TEXT NOT NULL UNIQUE,
+			username TEXT NOT NULL,
 			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL
 		);
