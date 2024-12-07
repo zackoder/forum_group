3b90f0ad45f34249d8e9af5a2fea0ae412b6a865
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-07 23:16:47 +0100

    create types structs for database tables

--- utils/types.go
+++ utils/types.go
@@ -1,2 +1,51 @@
 package utils
 
+type User struct {
+	Id       int
+	Username string
+	Email    string
+	Password string
+}
+
+type Session struct {
+	Id     int
+	UserId int
+	Token  string
+	Date   string
+}
+
+type Category struct {
+	Id   int
+	Name string
+}
+
+type Comment struct {
+	Id      int
+	UserId  int
+	PostId  int
+	Comment string
+	Date    string
+}
+
+type Post struct {
+	Id int
+	UserId int
+	Title string
+	Content string
+	Image string
+	Categories string
+	Date string
+}
+
+type Reaction struct {
+	Id int
+	UserId int
+	PostId int
+	CommentId int
+	Type string
+}
+
+type PostCategory struct {
+	PostId int
+	CategoryId int
+}
