47c133d1282f2521da2736e2732028dede129bca
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-07 13:27:32 +0100

    edit init_tables

--- models/init_tables.go
+++ models/init_tables.go
@@ -9,24 +9,6 @@ import (
 )
 
 func InitTables(db *sql.DB) {
-	UsersTable(db)
-	SessionsTable(db)
-	PostsTable(db)
-	CategoriesTable(db)
-	CommentsTable(db)
-	ReactionsTable(db)
-	PostsCategoriesTable(db)
-}
-
-func runQuery(db *sql.DB, query string) {
-	_, err := db.Exec(query)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-}
-
-func UsersTable(db *sql.DB) {
 	query := `
 		CREATE TABLE IF NOT EXISTS users(
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,24 +16,14 @@ func UsersTable(db *sql.DB) {
 			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL
 		);
-	`
-	runQuery(db, query)
-}
 
-func SessionsTable(db *sql.DB) {
-	query := `
 		CREATE TABLE IF NOT EXISTS sessions(
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			user_id INTEGER NOT NULL UNIQUE,
-			passkey TEXT NOT NULL UNIQUE,
+			token TEXT NOT NULL UNIQUE,
 			FOREIGN KEY (user_id) REFERENCES users(id)
 		);
-	`
-	runQuery(db, query)
-}
 
-func PostsTable(db *sql.DB) {
-	query := `
 		CREATE TABLE IF NOT EXISTS posts (
 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 			user_id INTEGER NOT NULL,
@@ -62,22 +34,12 @@ func PostsTable(db *sql.DB) {
 			date DATE NOT NULL,
 			FOREIGN KEY (user_id) REFERENCES users(id)
 		);
-	`
-	runQuery(db, query)
-}
-
-func CategoriesTable(db *sql.DB) {
-	query := `
+		
 		CREATE TABLE IF NOT EXISTS categories (
 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 			name varchar(255) NOT NULL UNIQUE
 		);
-	`
-	runQuery(db, query)
-}
 
-func CommentsTable(db *sql.DB) {
-	query := `
 		CREATE TABLE IF NOT EXISTS comments (
 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 			user_id INTEGER NOT NULL,
@@ -87,12 +49,7 @@ func CommentsTable(db *sql.DB) {
 			FOREIGN KEY (user_id) REFERENCES users(id),
 			FOREIGN KEY (post_id) REFERENCES posts(id)
 		);
-	`
-	runQuery(db, query)
-}
 
-func ReactionsTable(db *sql.DB) {
-	query := `
 		CREATE TABLE IF NOT EXISTS reactions (
 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 			user_id INTEGER NOT NULL,
@@ -107,19 +64,18 @@ func ReactionsTable(db *sql.DB) {
 				(post_id IS NOT NULL AND comment_id IS NULL)
 			)
 		);
-	`
-	runQuery(db, query)
-}
 
-func PostsCategoriesTable(db *sql.DB) {
-	query := `
 		CREATE TABLE IF NOT EXISTS posts_categories (
 			post_id INTEGER NOT NULL,
 			category_id INTEGER NOT NULL,
 			PRIMARY KEY (post_id,category_id),
 			FOREIGN KEY (post_id) REFERENCES posts(id),
 			FOREIGN KEY (category_id) REFERENCES categories(id)
-		)
+		);
 	`
-	runQuery(db, query)
+	_, err := db.Exec(query)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
