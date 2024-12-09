51db828a5d9bcf7a2fb20f39c6202d94cd44951c
walid-zouguagh <walid.zouguaagh@gmail.com>
2024-12-09 18:23:19 +0100

    fetch posts

--- api/api.go
+++ api/api.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
 	"forum/api/handlers"
+	"forum/utils"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -14,6 +18,13 @@ func main() {
 	mux.HandleFunc("/api/comments", handlers.Comments)
 	mux.HandleFunc("/api/posts", handlers.FetchPosts)
 	mux.HandleFunc("/api/reaction", handlers.Reactions)
+	var err error
+
+	utils.DB, err = sql.Open("sqlite3", "./database.sqlite")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 
 	/* run server */
 	fmt.Printf("server running on http://localhost%s\n", port)
--- api/handlers/comments.go
+++ api/handlers/comments.go
@@ -2,15 +2,32 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+
+	"forum/utils"
 )
 
-type test struct {
-	Name string
-	City string
-}
+// var MyDB *sql.DB
+
+// type test struct {
+// 	Name string
+// 	City string
+// }
 
 func Comments(w http.ResponseWriter, r *http.Request) {
-	t := &test{Name: "yassin", City: "oujda"}
-	json.NewEncoder(w).Encode(t)
+	query := `SELECT * FROM users;`
+	data, _ := utils.DB.Query(query)
+	var users []utils.User
+	for data.Next() {
+		var user utils.User
+		if err := data.Scan(&user.Id, &user.Username, &user.Email, &user.Password); err != nil {
+			fmt.Println(err)
+			return
+		}
+		users = append(users, user)
+	}
+
+	// t := &test{Name: "yassin", City: "oujda"}
+	json.NewEncoder(w).Encode(users)
 }
