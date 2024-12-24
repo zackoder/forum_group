c0731ffcc35236f78843b8ace2b641777f4827fc
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-24 15:53:18 +0100

    add some comments

--- router/web_router.go
+++ router/web_router.go
@@ -12,10 +12,10 @@ func WebRouter() *http.ServeMux {
 	webMux := http.NewServeMux()
 
 	webMux.HandleFunc("/static/", controllers.Server)
-	/* pages handlers */
+	/* --------------------------- pages handlers --------------------------- */
 	webMux.HandleFunc("/", controllers.Home)
 	webMux.HandleFunc("/add-post", middleware.Authorization(controllers.CreatePost))
-	/* login and register handlers */
+	/* --------------------------- login and register handlers --------------------------- */
 	webMux.HandleFunc("/Register", auth.RegisterUser)
 	webMux.HandleFunc("/Login", auth.SingIn)
 	webMux.HandleFunc("/register", controllers.Register)
