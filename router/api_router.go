c96ef961bf5fe06720d3f72d27231b30db174c9b
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-25 06:02:16 +0100

    fix page not found 404 status code

--- router/api_router.go
+++ router/api_router.go
@@ -13,10 +13,10 @@ func APIRouter() *http.ServeMux {
 	apiMux.HandleFunc(`/api/{PostId}/comments`, m.CheckMethod(api.Comments, "GET"))                    // comments list
 	apiMux.HandleFunc("/api/category/list", m.CheckMethod(api.CategoryList, "GET"))                    // get all categories
 	apiMux.HandleFunc("/api/posts", m.CheckMethod(api.FetchPosts, "GET"))                              // get all posts
-	apiMux.HandleFunc("/api/category/filter/{CategoryId}", m.CheckMethod(api.FilterByCategory, "GET")) // not complated
+	apiMux.HandleFunc("/api/category/filter/{CategoryId}", m.CheckMethod(api.FilterByCategory, "GET")) // filter by category
 
 	apiMux.HandleFunc("/api/{PostId}/comment/new", m.CheckMethod(m.Authorization(api.NewComment), "POST")) // create comment
-	apiMux.HandleFunc("/api/created/posts", m.CheckMethod(m.Authorization(api.CreatedPosts), "GET"))       // not complated
+	apiMux.HandleFunc("/api/created/posts", m.CheckMethod(m.Authorization(api.CreatedPosts), "GET"))       // filter by created posts
 	apiMux.HandleFunc("/api/liked/posts", m.CheckMethod(m.Authorization(api.LikedPosts), "GET"))           // not complated
 	apiMux.HandleFunc("/api/comment/reaction/{CommentId}", m.Authorization(api.CommentReaction))           // (like or dislike) a comment
 	apiMux.HandleFunc("/api/posts/reaction/{PostId}", m.Authorization(api.PostReaction))                   // create new post
