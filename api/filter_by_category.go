6fd20e929335bff3ec2d62fc895e274d8dc8e668
yrahhaou <yassinerahhaoui12@gmail.com>
2024-12-25 05:47:39 +0100

    complete

--- api/filter_by_category.go
+++ api/filter_by_category.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"strconv"
@@ -9,16 +10,22 @@ import (
 	"forum/utils"
 )
 
-type FilterPostsCategory struct {
-	UserId     int
-	Title      string
-	Content    string
-	Image      string
-	Categories string
-	Date       string
-}
-
 func FilterByCategory(w http.ResponseWriter, r *http.Request) {
+	/* ---------------------------- check if user is logged and get here id ---------------------------- */
+	var logged_user int
+	token, token_err := r.Cookie("token")
+	if token_err == nil {
+		query := `SELECT user_id FROM sessions WHERE token = ?;`
+		stmt, err := utils.DB.Prepare(query)
+		if utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		err = stmt.QueryRow(token.Value).Scan(&logged_user)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+	}
+
 	/* ------------------------------- Handle Category Id ------------------------------- */
 	id := r.PathValue("CategoryId")
 	category_id, num_err := strconv.Atoi(id)
@@ -44,10 +51,28 @@ func FilterByCategory(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var user_id int
 		var categories string
-		var p utils.PostsResult // this is a post im use p for short var
+		var p utils.PostsResult // this is a post i'm use p for short var
 		err := rows.Scan(&p.Id, &user_id, &p.Title, &p.Content, &categories, &p.Date, &p.UserName)
 		if utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
 			return
+		}
+		/* i'm not prepare this query because the post_id is not from user input */
+		get_likes := `SELECT COUNT(*) FROM reactions WHERE (post_id = ? AND type = "like")`
+		err = utils.DB.QueryRow(get_likes, p.Id).Scan(&p.Reactions.Likes)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		get_dislikes := `SELECT COUNT(*) FROM reactions WHERE (post_id = ? AND type = "dislike")`
+		err = utils.DB.QueryRow(get_dislikes, p.Id).Scan(&p.Reactions.Dislikes)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		if logged_user > 0 {
+			get_action := `SELECT type FROM reactions WHERE (user_id = ? AND post_id = ?)`
+			err = utils.DB.QueryRow(get_action, logged_user, p.Id).Scan(&p.Reactions.Action)
+			if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+				return
+			}
 		}
 		p.Categories = strings.Split(categories, ",")
 		posts = append(posts, p)
--- api/filter_created_posts.go
+++ api/filter_created_posts.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"strings"
@@ -35,12 +36,12 @@ func CreatedPosts(w http.ResponseWriter, r *http.Request) {
 	}
 
 	/* ---------------------------- Get Created Prosts ---------------------------- */
-	query = `SELECT p.id,p.title,p.content,p.categories,p.date,u.username FROM posts p JOIN users u ON p.user_id = ?`
+	query = `SELECT p.id,p.title,p.content,p.categories,p.date,u.username FROM posts p JOIN users u ON (p.user_id = ? AND u.id = ?)`
 	stmt, stmt_err = utils.DB.Prepare(query)
 	if utils.HandleError(utils.Error{Err: stmt_err, Code: http.StatusInternalServerError}, w) {
 		return
 	}
-	rows, rows_err := stmt.Query(user_id)
+	rows, rows_err := stmt.Query(user_id, user_id)
 	if utils.HandleError(utils.Error{Err: rows_err, Code: http.StatusInternalServerError}, w) {
 		return
 	}
@@ -49,6 +50,22 @@ func CreatedPosts(w http.ResponseWriter, r *http.Request) {
 		var categories string
 		row_err := rows.Scan(&p.Id, &p.Title, &p.Content, &categories, &p.Date, &p.UserName)
 		if utils.HandleError(utils.Error{Err: row_err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		/* i'm not prepare this query because the post_id is not from user input */
+		get_likes := `SELECT COUNT(*) FROM reactions WHERE (post_id = ? AND type = "like")`
+		err := utils.DB.QueryRow(get_likes, p.Id).Scan(&p.Reactions.Likes)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		get_dislikes := `SELECT COUNT(*) FROM reactions WHERE (post_id = ? AND type = "dislike")`
+		err = utils.DB.QueryRow(get_dislikes, p.Id).Scan(&p.Reactions.Dislikes)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
+			return
+		}
+		get_action := `SELECT type FROM reactions WHERE (user_id = ? AND post_id = ?)`
+		err = utils.DB.QueryRow(get_action, user_id, p.Id).Scan(&p.Reactions.Action)
+		if err != sql.ErrNoRows && utils.HandleError(utils.Error{Err: err, Code: http.StatusInternalServerError}, w) {
 			return
 		}
 		p.Categories = strings.Split(categories, ",")
