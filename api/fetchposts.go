e96bd58c4b3bbb6aff802ee64056e8ba48c7d7af
wzouguag <walid.zouguaagh@gmail.com>
2024-12-30 11:36:08 +0100

    cleaning code

--- api/fetchposts.go
+++ api/fetchposts.go
@@ -28,9 +28,7 @@ func FetchPosts(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(nil)
 		return
 	}
-	//, image, categories, date
 	query := "SELECT id, user_id, title, content, categories, date FROM posts ORDER BY id DESC LIMIT ? OFFSET ?"
-	// stm, err := utils.DB.Prepare(query)
 	rows, err := utils.DB.Query(query, 20, nbr_offset)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -38,34 +36,18 @@ func FetchPosts(w http.ResponseWriter, r *http.Request) {
 	}
 	defer rows.Close()
 	posts := []utils.PostsResult{}
-	// rows, err := stm.Query()
 	for rows.Next() {
-		// post := Post{}
 		var post utils.PostsResult
 		var user_id int
 		// var image sql.NullString
 		var date sql.NullString
 		var categories string
 
-		// type PostsResult struct {
-		// 	Id         int
-		// 	UserName   string
-		// 	Title      string
-		// 	Content    string
-		// 	Categories []string
-		// 	Date       string
-		// 	Reactions  struct {
-		// 		Likes    int
-		// 		Dislikes int
-		// 		Action   string
-		// 	}
-		// }
-
 		if err := rows.Scan(&post.Id, &user_id, &post.Title, &post.Content, &categories, &date); err != nil {
-			// log.Printf("Error scanning row %v", err)
-
-			fmt.Println(err)
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			// http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			error := Error{Message: http.StatusText(http.StatusInternalServerError), Code: http.StatusInternalServerError}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(error)
 			return
 		}
 
@@ -83,7 +65,10 @@ func FetchPosts(w http.ResponseWriter, r *http.Request) {
 		}
 
 		if err := utils.DB.QueryRow("SELECT username FROM users WHERE id = ?", user_id).Scan(&post.UserName); err != nil {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			// http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			error := Error{Message: http.StatusText(http.StatusInternalServerError), Code: http.StatusInternalServerError}
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(error)
 			return
 		}
 		// Get Likes the this post
@@ -106,7 +91,6 @@ func FetchPosts(w http.ResponseWriter, r *http.Request) {
 			get_action := `SELECT type FROM reactions WHERE (post_id = ? AND user_id = ?);`
 			err_action := utils.DB.QueryRow(get_action, post.Id, user_id).Scan(&post.Reactions.Action)
 			if err_action != nil {
-				// fmt.Println(err_action.Error())
 				if err_action == sql.ErrNoRows {
 					post.Reactions.Action = ""
 				} else {
